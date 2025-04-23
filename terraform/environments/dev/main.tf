5930a73e5875ca034f541dd4b4541e2304454109
Mircea Bardan <mircea.c.bardan@gmail.com>
2025-04-23 11:18:08 +0200

    add infra as code

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source              = "../../modules/vpc"
+  cidr_block          = "10.0.0.0/16"
+  name                = "dev"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones  = ["us-east-1a", "us-east-1b"]
+  public_subnet_count = 2
+  private_subnet_count = 2
+}
+
+module "ecs_cluster" {
+  source       = "../../modules/ecs-cluster"
+  cluster_name = "dev-cluster"
+}
